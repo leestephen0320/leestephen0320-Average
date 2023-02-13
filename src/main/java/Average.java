753d2d42998d1d4f0534e0740b8c78a1e8c86159
leestephen0320 <leestephen0320@gmail.com>
2023-02-13 04:01:20 +0000

    modified:   src/main/java/Average.java

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,10 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        int a = 0;
+        for(int i = 0; i < nums.length; i++;){
+            a = a + nums[i];
+        }
+        return a/nums.length;
     }
 }
